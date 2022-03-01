25c64b567c39af6cffc72d1052f5b81b7c479fee
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2022-03-02 00:47:02 +1100

    backend complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
 import { React, useState } from 'react';
 import styled from 'styled-components';
-import { validGuesses, validSolutions } from './source-code-arrays.js';
-import { CustomKeyboard } from './components/CustomKeyboard';
+import { validSolutions } from './source-code-arrays.js';
 import { LetterMatrix } from './components/LetterMatrix';
 
 const App = () => {
 
-  // These 3 arrays should be mutually exclusive
+  const [possibleWords, setPossibleWords] = useState('')
   const [form, setForm] = useState({
     correctLetters: Array(5).fill(''),
     presentLetters: Array(5).fill(''),
     absentLetters: Array(12).fill(''),
   })
 
-  const [possibleWords, setPossibleWords] = useState([])
-
-  // Check mutual exclusivity here later
   const handleChange = (e, index) => {
     const key = e.target.name + 'Letters'
-    form[key][index] = e.target.value
+    form[key][index] = e.target.value.toLowerCase()
     setForm((prevState) => {
       return {
         ...prevState,
         [key]: form[key]
       }
     })
-    console.log(form)
   }
 
   const handleReset = () => {
@@ -34,44 +29,59 @@ const App = () => {
       presentLetters: Array(5).fill(''),
       absentLetters: Array(12).fill(''),
     })
+    setPossibleWords('')
   }
 
   const solve = () => {
-    console.log("Valid Solutions:", validSolutions.length)
-    console.log("Valid Guesses:\t", validGuesses.length)
+    let absentLetters = form.absentLetters.filter(l => l !== '')
+    absentLetters = [...new Set(absentLetters)]
 
-    // setPossibleWords()
+    const correctLetters = form.correctLetters
+    const presentLetters = form.presentLetters
+
+    let filteredSolutions = validSolutions
+
+    absentLetters.forEach(al => {
+      filteredSolutions = filteredSolutions.filter(w => !w.includes(al))
+    })
+
+    for (let i = 0; i < 5; i++) {
+      if (correctLetters[i]) {
+        filteredSolutions = filteredSolutions.filter(w => w[i] === correctLetters[i])
+      }
+      if (presentLetters[i]) {
+        filteredSolutions = filteredSolutions.filter(w => w.includes(presentLetters[i]) && w[i] !== presentLetters[i])
+      }
+    }
+    
+    if (filteredSolutions.length > 0) {
+      setPossibleWords(filteredSolutions.join(', '))
+    } else {
+      setPossibleWords('Could not find any possible words. :(')
+    }
   }
 
   return (
       <Container>
         <h1>Wordle Solver</h1>
-
-        <p>Note: Letter position matters for correct and present letters</p>
-
-        <label htmlFor="correct">Enter correct letters:</label>
+        <label htmlFor="correct">Enter correct letters*</label>
         <LetterMatrix type='correct' letters={form.correctLetters} setLetters={handleChange} />
-        <br />
-        <label htmlFor="present">Enter present letters:</label>
+
+        <label htmlFor="present">Enter present letters*</label>
         <LetterMatrix type='present' letters={form.presentLetters} setLetters={handleChange} />
-        <br />
-        <label htmlFor="absent">Enter absent letters:</label>
+
+        <label htmlFor="absent">Enter absent letters</label>
         <LetterMatrix type='absent' letters={form.absentLetters} setLetters={handleChange} />
         
-        <br />
-        <div className="btn-group">
-          <button onClick={handleReset}>Reset</button>
-          <button onClick={solve}>Solve</button>
+        <div>
+          <Button onClick={handleReset}>Reset</Button>
+          <Button onClick={solve}>Solve</Button>
         </div>
-        <br />
-        
+        <Disclaimer>*Letter position matters for correct and present letters</Disclaimer>
         <label htmlFor="possible-words">Possible words</label>
-        <Result type="text" name="possible-words" id="possible-words" readOnly value={possibleWords}/>  
-        <br />
-        <br />
-        {/* Additions */}
-        {/* <CustomKeyboard /> */}
+        <Result type="text" name="possible-words" id="possible-words" readOnly value={possibleWords}/>
 
+        <Link href="https://github.com/adriantan09/wordle-solver" target="_blank">About</Link>
       </Container>
   );
 }
@@ -80,18 +90,46 @@ export default App;
 
 const Container = styled.div`
   text-align: center;
-  color: white;
-  background-color: #282c34;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media (prefers-color-scheme: light) {
+    h1, label, p, a { color: black; }
+    background-color: #FFFBFB;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    h1, label, p, a { color: white; }
+    background-color: #282c34;
+  }
+`
+
+const Disclaimer = styled.p`
+  font-size: 11pt;
+`
+
+const Button = styled.button`
+  font-size: 12pt;
+  margin: 10px 5px 0 5px;
+  line-height: 32px;
+  padding: 0 20px;
+  cursor: pointer;
+  color: #000;
+  background: #fff;
 `
 
 const Result = styled.textarea`
-  width: 400px;
+  width: 90%;
+  max-width: 350px;
   height: 100px;
-  border-radius: 8px;
-  border: none;
+  font-size: 12pt;
+  text-align: center;
+  padding: 0 5px;
+`
+
+const Link = styled.a`
+  text-decoration: none;
 `
--- src/components/LetterMatrix.jsx
+++ src/components/LetterMatrix.jsx
@@ -7,38 +7,34 @@ export const LetterMatrix = ({ type, letters, setLetters }) => {
     // letters only
     const re = /^[A-Za-z]{1}$/g;
     const isLetter = re.test(e.key)
-    console.log(e.key)
-    if (!(isLetter || e.key == 'Backspace')) {
-      console.log('no')
-      e.preventDefault();
-    }
-    // handleTab(e, isLetter)
-  }
-
-  const handleTab = (e, isLetter) => {
-      // const form = e.target.form;
-      // let index = [...form].indexOf(e.target);
-      // console.log('form', form) 
-
-      // if (e.key == 'Backspace') index -= 1
-      // if (isLetter) index += 1
-
-      // // e.target.value != '' ? index += 1 : index -= 1
-      // const nextElement = form.elements[index]
-      // nextElement?.focus();
-      // e.preventDefault();
+    if (!isLetter) e.preventDefault();
   }
 
   return ( 
-    <form>
+    <Form>
         {[...Array(letters.length)].map((x, i) => (
-            <LetterBox key={i} className='letter' name={type} maxLength={1} onChange={e => setLetters(e, i)} value={letters[i].toUpperCase()}/>
+            <LetterBox 
+              key={i}
+              className='letter'
+              name={type}
+              maxLength={1}
+              onKeyPress={handleInput}
+              onChange={e => setLetters(e, i)}
+              value={letters[i].toUpperCase()}
+              autoComplete='off'
+            />
         ))}
-    </form>
+    </Form>
   )
 }
 
 const LetterBox = styled.input`
-  width: 40px;
-  height: 40px;
+  width: 39px;
+  height: 39px;
+  text-align: center;
+  font-size: 20pt;
+`
+
+const Form = styled.form`
+  max-width: 300px;
 `
